49397433c3b0dc3bdaa2418bc37d0c0425de8c33
JChien <jeffchien13@gmail.com>
2015-11-07 01:55:16 +0800

    add entrypoint
    Signed-off-by: JChien <jeffchien13@gmail.com>

---
+++ kvctl.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/codegangsta/cli"
+	"github.com/jeffchien/kvctl/command"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "kvctl"
+	app.Usage = "manage kv"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "backend",
+			Value:  "consul://127.0.0.1:8500",
+			Usage:  "backend kv storage path",
+			EnvVar: "KVCTL_BACKEND",
+		},
+	}
+	app.Run(os.Args)
+}
